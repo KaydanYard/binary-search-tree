77be3deeb124e94ed65da14f91aa6f01f11c2acb
Kaydan Yardley <kaydan@Kaydans-MacBook-Pro.local>
2021-11-08 07:42:15 -0700

    finished

---
+++ spec/sorter.spec.js
@@ -0,0 +1,7 @@
+describe('selection-sort', () => {
+  it('should sort an array of 100 items', () => {
+    let array = Array.from({length: 100}, () => 
+      Math.floor(Math.random())
+    );
+  })
+})
---
+++ src/sorter.js
@@ -0,0 +1,20 @@
+function selectionSort(array) {
+  for (let index = 0; index < array.length; index++) {
+    let minItem = array[index];
+    let minItemIndex = index;
+
+    for (let searchIndex = 0; searchIndex < array.length; searchIndex++) {
+      const item = array[searchIndex];
+      
+      if (item < minItem) {
+        minItem = item;
+        minItemIndex = searchIndex;
+      }
+    }
+
+    const temp = array[index];
+    array[index] = minItem;
+    array[minItemIndex] = temp;
+  }
+  return array;
+}
